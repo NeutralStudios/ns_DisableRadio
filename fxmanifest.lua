a8bd2db01accd2e5568a1ce9114ccfcd296393bc
Neutral Studios <support.neutralstudios@gmail.com>
2026-06-06 14:46:53 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+--- This Resource was made by Neutral Studios ---
+-- Discord 'https://discord.gg/hrFcY96tCT'
+-- Tebex 'https://neutral.tebex.io'
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Neutral Studios'
+description 'Disable the GTA5 Basic Radio System'
+version '1.0.0'
+
+client_script {
+    'client/main.lua'
+}
